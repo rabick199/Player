65331cd9a478bd35d9c1bc6c48fd80b73327d976
rabick199 <30829650+rabick199@users.noreply.github.com>
2018-02-08 11:27:12 +0000

    Create 30_char.c

---
+++ 30_char.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    char str[100],s[100];
+    int i,count=0,k;
+    printf("Enter strings:");
+    scanf("%s%s",str[i],s[i]);
+    printf("\n Enter k:");
+    scanf("%d",&k);
+    for(i=0;str[i])
+    
+}
