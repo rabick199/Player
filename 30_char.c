37c4f15edc2c5a9a2d2dd4833e88d3e12d49164a
rabick199 <30829650+rabick199@users.noreply.github.com>
2018-02-13 16:16:19 +0530

    Update 30_char.c

--- 30_char.c
+++ 30_char.c
@@ -1,30 +1,20 @@
+
 #include<stdio.h>
 void main()
 {
-    char str[100],s[100];
-    int i,count=0,k;
-    printf("Enter strings:");
-    scanf("%s",&s[i]);
-    printf("\n Enter string2:");
-    scanf("%s",&str[i]);
-    printf("\n Enter k:");
-    scanf("%d",&k);
-    for(i=0;str[i]='\0'||s[i]!='\0';i++)
-    {
-        if(str[i]!=s[i])
-        {
-            count++;
-        }
-    }
-    printf("%d",count);
-        if(k==count)
-        {
-            printf("\n Yes");
-        }
-        else
-        {
-            printf("\n No");
-        }
-    
-    
+char str[100],s[100];
+int i,count=0,k;
+printf("Enter two strings:");
+scanf("%s%s",&str,&s);
+printf("\n Enter the K:");
+scanf("%d",&k);
+for(i=0;str[i]!='\0'||s[i]!='\0';i++)
+{
+if(s[i]!=str[i])
+count++;
+}
+if(count==k)
+printf("Yes");
+else
+printf("No");
 }
