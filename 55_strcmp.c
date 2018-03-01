f5100e871486b0e15778baca5f3022fcce7c1490
rabick199 <30829650+rabick199@users.noreply.github.com>
2018-03-01 16:35:28 +0530

    Create new file

---
+++ 55_strcmp.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+#include<string.h>
+#include<conio.h>
+void main()
+{
+	char a[100],b[100];
+	int i,l,m,count=0;
+	printf("Enter the strings:");
+	gets(a);
+	gets(b);
+	l=strlen(a);
+	m=strlen(b);
+	if(l!=m)
+	{
+		printf("Not equal");
+	}
+	else
+	{
+		for(i=0;i<m;i++)
+		{
+			a[i]=tolower(a[i]);
+			b[i]=tolower(b[i]);
+			if(a[i]==b[i])
+			{
+				count++;
+			}
+		}
+		if(count==m)
+		{
+			printf("Equal");
+		}
+		else
+		{
+			printf("Not Equal");
+		}
+	}
+}
