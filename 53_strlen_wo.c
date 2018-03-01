552812e5946392b36ef0bf8e0f162ae8526dc238
rabick199 <30829650+rabick199@users.noreply.github.com>
2018-03-01 16:12:07 +0530

    Create 53_strlen_wo.c

---
+++ 53_strlen_wo.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	char str[100];
+	int i;
+	printf("Enter the string:");
+	gets(str);
+	for(i=0;str[i]!='\0';i++)
+	{
+	}
+	printf("\n The string length is:%d",i);
+}
