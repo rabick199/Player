58fb8bc5e143272291cc2aeca42477ca89089786
rabick199 <30829650+rabick199@users.noreply.github.com>
2018-03-01 16:03:54 +0530

    Create new file

---
+++ 52_Kth_small.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+void main()
+{
+	int a[100],n,i,j,t,k;
+	printf("Enter the N:");
+	scanf("%d",&n);
+	printf("\nEnter K:");
+	scanf("%d",&k);
+	printf("\n Enter the array:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for(i=0;i<n-1;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+		  if(a[i]>a[j])
+		  {
+		  	t=a[i];
+		  	a[i]=a[j];
+		  	a[j]=t;
+			  }	
+		}
+	}
+	printf("\n The second smallest number:%d",a[k-1]);
+	
+}
